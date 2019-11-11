a7da052e3a6451dff2b180f06ad10c3e30d44365
T-Oner <1@fxcdev.com>
2019-11-11 22:28:16 +0800

    Initial commit

---
+++ app/src/main/java/com/toner/catchit/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.toner.catchit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toner.catchit.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
